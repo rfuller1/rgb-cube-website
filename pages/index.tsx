600475c35fec52017191648a860b0c44c556964f
rfuller1 <60265028+rfuller1@users.noreply.github.com>
2022-12-11 20:05:53 -0500

    had to revert some styling because of issues with react

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,97 +23,71 @@ export default function Home() {
 
 
   const handleSubmit = (event : any) => {
-      event.preventDefault(); // prevent page refresh
-      fetch("https://rgb-led-app.herokuapp.com/api/hello", {
-      method: "POST",
-      mode: "no-cors",
-      body: JSON.stringify({name: name, latitude: lati, longitude:longi})
-    })
-    setName(""); // reset form
-    setLati("");
-    setLongi("");
+       event.preventDefault(); // prevent page refresh
+       fetch("https://rgb-led-app.herokuapp.com/api/hello", {
+       method: "POST",
+       mode: "no-cors",
+       body: JSON.stringify({name: name, latitude: lati, longitude:longi})
+     })
+     setName(""); // reset form
+     setLati("");
+     setLongi("");
 
 
-    };
+     };
 
-  return (
+   return (
+     <div className={styles.container}>
+       <main className={styles.main}>
+         <h1 className={styles.title}>
+           RGB Weather Cube
+         </h1>
 
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          RGB Weather Cube
-        </h1>
+         <p className={styles.description}>
+           Get started by telling us the location you want to get your weather from.
+         </p>
+         <div>
 
-        <p className={styles.description}>
-          Get started by telling us the location you want to get your weather from.
-        </p>
-
-
-        <div>
-
-   <form onSubmit={handleSubmit}>
-   <Grid
-   container
- direction="column"
- justifyContent="center"
- alignItems="center"
-
->
-     <TextField
-       label="Name of Arduino" variant="filled" color="secondary" sx={{ input: { color: 'white' } }} focused
-       inputProps={{style: {fontSize: 25}}}
-       InputLabelProps={{style: {fontSize: 25}}}
-       id="longitude"
-       name="longitude"
-       id="first_name"
-       name="first_name"
-       type="text"
-       onChange={event => setName(event.target.value)}
-       value={name}
-     />
-     <Box sx={visuallyHidden}>Enter the name of the Arduino you are connecting to here.</Box>
-
-      <TextField
-      label="Latitude" variant="filled" color="success" sx={{ input: { color: 'white' } }} focused
-      inputProps={{style: {fontSize: 25}}}
-      InputLabelProps={{style: {fontSize: 25}}}
-       id="latitude"
-       name="latitude"
-       type="text"
-       pattern="^-?[0-9]\d{0,1}(\.\d+)?$"
-       onChange={event => setLati(event.target.value)}
-       value={lati}
-     />
-     <Box sx={visuallyHidden}>Enter the latitude of the location whose weather you want to view.</Box>
-
-     <TextField
-
-       label="Longitude"
-       inputProps={{style: {fontSize: 25}}}
-       InputLabelProps={{style: {fontSize: 25}}}
-       variant="filled" color="primary" sx={{input: { color: 'white' }}} focused
-       id="longitude"
-       name="longitude"
-       type="text"
-       pattern="^-?[0-9]\d{0,1}(\.\d+)?$"
-       onChange={event => setLongi(event.target.value)}
-       value={longi}
-     />
-     <Box sx={visuallyHidden}>Enter the longitude of the location whose weather you want to view.</Box>
-     <Box m={2} pt={3}>
-
-     <Button sx={{ color: 'white', borderColor: 'white' }} inputProps={{style: {fontSize: 25}}}
-     InputLabelProps={{style: {fontSize: 25}}} type="submit" variant="outlined">Submit</Button>
-     </Box>
+    <form onSubmit={handleSubmit}>
 
 
-</Grid>
-   </form>
- </div>
+    <label> Name </label>
 
-      </main>
+      <input className={styles.form_input}
+        id="first_name"
+        name="first_name"
+        type="text"
+        onChange={event => setName(event.target.value)}
+        value={name}
+      />
 
-    </div>
 
-  )
-}
+<label> Latitude </label>
+      <input className={styles.form_input}
+        id="latitude"
+        name="latitude"
+        type="text"
+        pattern="^-?[0-9]\d{0,1}(\.\d+)?$"
+        onChange={event => setLati(event.target.value)}
+        value={lati}
+      />
+<label> Longitude </label>
+      <input className={styles.form_input}
+        id="longitude"
+        name="longitude"
+        type="text"
+        pattern="^-?[0-9]\d{0,1}(\.\d+)?$"
+        onChange={event => setLongi(event.target.value)}
+        value={longi}
+      />
+    <button className={styles.button} type="submit">Submit form</button>
+
+    </form>
+
+  </div>
+
+       </main>
+
+     </div>
+   )
+ }
