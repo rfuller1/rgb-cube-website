0688b867dd816fdcbc24591cfc4eba54181185fd
rfuller1 <60265028+rfuller1@users.noreply.github.com>
2022-12-11 19:30:36 -0500

    improved style

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,16 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 import {useState} from 'react';
+import Button from '@mui/material/Button';
+import Input from '@mui/material/Input';
+import TextField from  '@mui/material/TextField';
+import Box from  '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Grid';
+import { visuallyHidden } from '@mui/utils';
+
+
+
 
 
 
@@ -27,6 +37,7 @@ export default function Home() {
     };
 
   return (
+
     <div className={styles.container}>
       <main className={styles.main}>
         <h1 className={styles.title}>
@@ -36,18 +47,36 @@ export default function Home() {
         <p className={styles.description}>
           Get started by telling us the location you want to get your weather from.
         </p>
+
+
         <div>
 
    <form onSubmit={handleSubmit}>
-     <input
+   <Grid
+   container
+ direction="column"
+ justifyContent="center"
+ alignItems="center"
+
+>
+     <TextField
+       label="Name of Arduino" variant="filled" color="secondary" sx={{ input: { color: 'white' } }} focused
+       inputProps={{style: {fontSize: 25}}}
+       InputLabelProps={{style: {fontSize: 25}}}
+       id="longitude"
+       name="longitude"
        id="first_name"
        name="first_name"
        type="text"
        onChange={event => setName(event.target.value)}
        value={name}
      />
+     <Box sx={visuallyHidden}>Enter the name of the Arduino you are connecting to here.</Box>
 
-     <input
+      <TextField
+      label="Latitude" variant="filled" color="success" sx={{ input: { color: 'white' } }} focused
+      inputProps={{style: {fontSize: 25}}}
+      InputLabelProps={{style: {fontSize: 25}}}
        id="latitude"
        name="latitude"
        type="text"
@@ -55,8 +84,14 @@ export default function Home() {
        onChange={event => setLati(event.target.value)}
        value={lati}
      />
+     <Box sx={visuallyHidden}>Enter the latitude of the location whose weather you want to view.</Box>
 
-     <input
+     <TextField
+
+       label="Longitude"
+       inputProps={{style: {fontSize: 25}}}
+       InputLabelProps={{style: {fontSize: 25}}}
+       variant="filled" color="primary" sx={{input: { color: 'white' }}} focused
        id="longitude"
        name="longitude"
        type="text"
@@ -64,12 +99,21 @@ export default function Home() {
        onChange={event => setLongi(event.target.value)}
        value={longi}
      />
-     <button type="submit">Submit form</button>
+     <Box sx={visuallyHidden}>Enter the longitude of the location whose weather you want to view.</Box>
+     <Box m={2} pt={3}>
+
+     <Button sx={{ color: 'white', borderColor: 'white' }} inputProps={{style: {fontSize: 25}}}
+     InputLabelProps={{style: {fontSize: 25}}} type="submit" variant="outlined">Submit</Button>
+     </Box>
+
+
+</Grid>
    </form>
  </div>
 
       </main>
 
     </div>
+
   )
 }
